98befa1c370cca19b9441d7606bc4d0e3caa905a
Junwoong An <ajx0114@naver.com>
2023-03-03 11:39:26 +0900

    add size capacity reserve

--- 1_seq/vector.h
+++ 1_seq/vector.h
@@ -20,22 +20,51 @@ vector_t<T>::vector_t() :
 template<typename T>
 inline vector_t<T>::vector_t(const vector_t& v)
 {
+    // copy
+    // array = new T[s]();
 }
 
 template<typename T>
-inline vector_t<T>::~vector_t() {}
+inline vector_t<T>::~vector_t() { free(array); }
 
+
+template<typename T>
+inline size_t vector_t<T>::size() const
+{
+    return num_elements;
+}
 
 template<typename T>
 inline bool vector_t<T>::empty() const
 {
-    cout << "test empty : " << static_cast<int>((sizeof(array) / sizeof(*array))) << " type is : " << typeid((static_cast<int>(sizeof(array) / sizeof(*array)))).name() << endl;
-    this.array_size = static_cast<int>((sizeof(array) / sizeof(*array)));
-    if (array_size == 0) {
+    if (num_elements == 0) {
         return true;
     }
     else {
         return false;
+    }
+}
+
+template<typename T>
+inline size_t vector_t<T>::capacity() const
+{
+    return array_size();
+}
+
+template<typename T>
+inline void vector_t<T>::reserve(size_t s)
+{
+    if (array_size < s) {
+        array_size = s;
+        T* newArray = (T*)malloc(s * sizeof(T));
+
+        for (size_t i = 0; i < array_size; i++)
+        {
+            newArray[i] = array[i];
+        }
+        free(array);
+        array = newArray;
+        free(newArray);
     }
 }
 
