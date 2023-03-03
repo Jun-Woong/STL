e649a09ec0b9d20c253ac9a92b53087ec3d81187
Junwoong An <ajx0114@naver.com>
2023-03-03 14:25:15 +0900

    fix pop_back 20

--- 1_seq/vector.h
+++ 1_seq/vector.h
@@ -106,7 +106,7 @@ inline void vector_t<T>::pop_back()
         {
             newArray[i] = array[i];
         }
-        //free(array);
+        free(array);
         //array = newArray;
         //newArray = NULL; // if free, error
     }
