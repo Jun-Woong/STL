9acb49857b068ed7fad8e4aed2ebfece17452cf4
Junwoong An <ajx0114@naver.com>
2023-03-03 11:12:22 +0900

    fix previous

--- 1_seq/vector.h
+++ 1_seq/vector.h
@@ -28,7 +28,7 @@ inline vector_t<T>::~vector_t() {}
 template<typename T>
 inline bool vector_t<T>::empty() const
 {
-    cout << "test empty : " << (sizeof(array) / sizeof(*array)) << " type is : " typeid(sizeof(array) / sizeof(*array)) << endl;
+    cout << "test empty : " << (sizeof(array) / sizeof(*array)) << " type is : " << typeid(sizeof(array) / sizeof(*array)) << endl;
     if (array_size == 0) {
         return true;
     }
