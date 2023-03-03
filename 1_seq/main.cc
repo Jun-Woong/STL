5002136513b4be16360d8d644bd6b9f0d03eb3e3
Junwoong An <ajx0114@naver.com>
2023-03-03 13:35:07 +0900

    fix push_back 3

--- 1_seq/main.cc
+++ 1_seq/main.cc
@@ -26,6 +26,12 @@ int main () {
     cout << endl;
 
     v1.push_back("16");
+
+    for (size_t i = 0; i < v1.size(); i++) {
+        cout << "v1[" << i << "]  : " << v1[i] << endl; //여기서 operator[] 사용
+    }
+    cout << endl;
+
     //v1.push_back("32");
     //v1.push_back("64");
     //cout << endl;
