7512970244b30b89665d32916aa204e51e9090c0
Junwoong An <ajx0114@naver.com>
2023-03-03 13:46:54 +0900

    fix pop_back 3

--- 1_seq/vector.h
+++ 1_seq/vector.h
@@ -109,7 +109,7 @@ inline void vector_t<T>::pop_back()
         newArray = (T*)malloc(num_elements * sizeof(T));
         for (size_t i = 0; i < num_elements; i++)
         {
-            cout << "array[i] : " << array[i] << endl;
+            cout << "i : " << array[i] << endl;
             newArray[i] = array[i];
         }
         free(array);
