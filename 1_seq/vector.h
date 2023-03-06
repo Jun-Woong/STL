9a56489bf988e449a57bd13da4f2313514e0bc92
Junwoong An <ajx0114@naver.com>
2023-03-06 11:31:04 +0900

    fix insert

--- 1_seq/vector.h
+++ 1_seq/vector.h
@@ -158,14 +158,14 @@ inline iterator_t<T> vector_t<T>::insert(const iterator pos, const T& v)
         newArray = NULL;
     }
     else {
+        cout << "!!!!!!!! " << num_elements << endl;
         for (size_t i = 0; i < (num_elements); i++)
         {
-            cout << " AAAAAA " << endl;
             if (pos.ptr == (array + i)) {
-                cout << " BBBBB " << endl;
                 num_elements++;
                 T tmp = array[i];
                 array[i] = v;
+                cout << " @@@@ " << i << " ####### " << array[i] << endl;
                 T now;
                 for (size_t j = i + 1; j < num_elements; j++)
                 {
