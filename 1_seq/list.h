bc5ebb49362658fbc2a8f1bf54f5db2e68d804bf
Junwoong An <ajx0114@naver.com>
2023-03-06 19:22:13 +0900

    fix list

--- 1_seq/list.h
+++ 1_seq/list.h
@@ -57,7 +57,8 @@ inline void list_t<T>::push_back(const T& d)
         cout << " BBBBBBBB " << endl;
     }
     else {
-        list_element<T> ne = (list_element<T>*)malloc(sizeof(list_element<T>));
+        list_element<T> ne;
+        ne = (list_element<T>*)malloc(sizeof(list_element<T>));
         cout << " CCCCCCCCC " << endl;
         ne.left = array[num_elements - 1].right;
         cout << " DDDDDDDDD " << endl;
