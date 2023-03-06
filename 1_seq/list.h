bc3bd43b3ef81bfc757ad6f8f5a4541425c1028e
Junwoong An <ajx0114@naver.com>
2023-03-06 18:05:24 +0900

    fix list

--- 1_seq/list.h
+++ 1_seq/list.h
@@ -48,7 +48,7 @@ inline bool list_t<T>::empty() const
 template<typename T>
 inline void list_t<T>::push_back(const T& d)
 {
-    if (array.size() == 0) {
+    if (array.length == 0) {
         array[0].value = d;
     }
     else {
