2344b7141813ac54ec571b884e60e48b77c86e68
Junwoong An <ajx0114@naver.com>
2023-03-07 18:16:40 +0900

    fix

--- 1_seq/list.h
+++ 1_seq/list.h
@@ -110,11 +110,11 @@ inline void list_t<T>::operator=(const list_t<T>& v)
     list_element<T>* tmp = v.head;
     list_element<T>* ntmp = head;
     while (tmp->right != NULL) {
+        ntmp = ntmp->right;
         list_element<T>* newElement = new list_element<T>;
         ntmp->right = newElement;
         newElement->value = tmp->value;
         tmp = tmp->right;
-        ntmp = ntmp->right;
     }
 }
 
