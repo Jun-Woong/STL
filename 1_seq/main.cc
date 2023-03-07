30b4bf938adb6586503a37667f098c7bb77c6610
Junwoong An <ajx0114@naver.com>
2023-03-07 14:25:52 +0900

    check begin

--- 1_seq/main.cc
+++ 1_seq/main.cc
@@ -121,7 +121,7 @@ int main () {
     cout << "size   : " << l1.size() << endl; 
     cout << endl;
 
-    //l1.push_back("2");
+    l1.push_back("2");
     //l1.push_back("4");
     //l1.push_front("8");
     //l1.push_front("16");
