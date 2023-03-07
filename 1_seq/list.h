d45ed20990c97fbae2627d220b253d851f5abd87
Junwoong An <ajx0114@naver.com>
2023-03-07 16:52:40 +0900

    fix

--- 1_seq/list.h
+++ 1_seq/list.h
@@ -166,7 +166,7 @@ inline iterator_l<T> list_t<T>::erase(const iterator pos)
         head = pos.ptr->right;
     }
     else {
-        cout << " VVVVVVV " << (pos.ptr->left)->right << endl;
+        cout << " VVVVVVV " << (pos.ptr->left) << endl;
         pos.ptr->left->right = pos.ptr->right;
         cout << "VVVVVVV" << endl;
         pos.ptr->right->left = pos.ptr->left;
