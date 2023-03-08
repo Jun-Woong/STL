9241e93c18c40ddf5a5e690c75825257cc8fc637
Junwoong An <ajx0114@naver.com>
2023-03-08 13:30:03 +0900

    test sort

--- 1_seq/main.cc
+++ 1_seq/main.cc
@@ -147,15 +147,14 @@ int main () {
     cout << endl;
 
     l2 = l1;
+    l1.clear();
+    list_t<string> l3(l2);
+    l2.sort();
 
     for (list_t<string>::iterator it = l2.begin(); it != l2.end(); it++) {
         cout << "%%%%it    : " << *it << endl;
     }
     cout << endl;
-
-    l1.clear();
-    list_t<string> l3(l2);
-    l2.sort();
 
 //
 //    l2.merge(l3);
