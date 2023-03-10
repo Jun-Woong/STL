02876af23faf172db1b033b3e2d39cb0a2fee51b
Junwoong An <ajx0114@naver.com>
2023-03-10 09:39:48 +0900

    fix

--- 2_adp/pri_queue.h
+++ 2_adp/pri_queue.h
@@ -22,7 +22,18 @@ inline priority_queue<T, V, F>::~priority_queue() {}
 template <typename T, typename V, typename F>
 size_t priority_queue<T, V, F>::size() const
 {
-    return queue.size();
+    queue.push_back(v);
+    cmp c;
+    T tmp;
+    for (size_t i = 0; i < queue.size(); i++) {
+        for (size_t j = 1; j < queue.size(); j++) {
+            if (c(queue[i], queue[j])) {
+                tmp = queue[i];
+                queue[i] = queue[j];
+                queue[j] = tmp;
+            }
+        }
+    }
 }
 
 template <typename T, typename V, typename F>
@@ -45,7 +56,7 @@ inline T& priority_queue<T, V, F>::top() const
 template <typename T, typename V, typename F>
 inline void priority_queue<T, V, F>::push(const T& v)
 {
-    queue.push_back(v);
+    queue.push_back(v); // fix here tomorrow
 }
 
 template <typename T, typename V, typename F>
