0e2516e0463fb60499ee765a666e6f8e165a10de
Junwoong An <ajx0114@naver.com>
2023-03-12 15:46:38 +0900

    fix

--- 3_asc/main.cc
+++ 3_asc/main.cc
@@ -77,9 +77,9 @@ int main () {
     m1[2] = " ";
     m1[3] = "wor";
     m1[4] = "ld";
-    map_t<int, string>::iterator* it2;
+    map_t<int, string>::iterator it2;
     for (it2 = m1.begin(); it2 != m1.end(); ++it2){
-        cout << "*it2 k : " << 1 << endl;
+        cout << "*it2 k : " << it2.first << endl;
     }
     cout << endl;
     //for (it2 = m1.begin(); it2 != m1.end(); ++it2){
