b66c01c737711153be7aaeb63e107c4a45df45cd
Junwoong An <ajx0114@naver.com>
2023-03-12 18:47:41 +0900

    asdf

--- 3_asc/map.h
+++ 3_asc/map.h
@@ -248,7 +248,7 @@ inline iterator_m<K, V> map_t<K, V>::find(const K& d)
     //return iter;
 
     iterator_m<K, V> iter;
-    std::cout << " 12312123 " << head->right << "\n";
+    std::cout << " 12312123 " << head->right->value << "\n";
     iter.ptr = head->left;
     return iter;
 }
