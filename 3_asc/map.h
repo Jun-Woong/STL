47bc381aaaba7848eef1306cdcca06abed942431
Junwoong An <ajx0114@naver.com>
2023-03-12 18:22:07 +0900

    asdf

--- 3_asc/map.h
+++ 3_asc/map.h
@@ -208,6 +208,7 @@ inline iterator_m<K, V> map_t<K, V>::erase(const iterator& pos)
                 it.ptr->left->right = NULL;
             }
             else {
+                std::cout << " @@@@@@ " << "\n";
                 it.ptr->left->right = it.ptr->right;
                 it.ptr->right->left = it.ptr->left;
             }
